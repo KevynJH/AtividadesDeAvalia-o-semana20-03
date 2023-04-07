ef7b9832833f836d878d43affb44c10009571171
KevynJH <kevynjhonatanb@gmail.com>
2023-04-06 21:55:50 -0300

    06/04/2023

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 package Atividaderevisao1;
 
 public class App {
-    
+    public static void main(String[] args) {
+        ExemploRevisao1 obj = new ExemploRevisao1();
+        obj.exemplo2();
+    }
 }
--- ExemploRevisao.java
+++
@@ -1,5 +0,0 @@
-package Atividaderevisao1;
-
-public class ExemploRevisao {
-    
-}
